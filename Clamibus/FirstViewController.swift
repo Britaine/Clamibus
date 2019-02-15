22c47615c03897c2068ea78529ba7360d83bbfe1
Pierre Crespi <526473+gb96@users.noreply.github.com>
2019-02-15 01:37:52 +0100

    Initial Commit

---
+++ Clamibus/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Clamibus
+//
+//  Created by  Pierre Crespi on 15/02/2019.
+//  Copyright © 2019  Pierre Crespi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
